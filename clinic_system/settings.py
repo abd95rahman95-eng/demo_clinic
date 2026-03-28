056b64b154c16fe0f6ccd51eccf5b1cf719dbca7
Abd <abd95rahman95@gmail.com>
2026-03-28 09:45:37 +0100

    create admin user

--- clinic_system/settings.py
+++ clinic_system/settings.py
@@ -121,3 +121,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     }
 }
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@test.com', 'Admin12345')
